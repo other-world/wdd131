2eec9d154f4451d8cfc948460aee62a5737c2e86
other-world <jamie@other-world.org>
2024-07-06 22:57:27 -0600

    Start of Week02 project, lots of bugs still.

---
+++ week02/scripts/temples.js
@@ -0,0 +1,18 @@
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('_______', () => {
+	navigation._______.toggle('_______');
+	hamButton.classList.toggle('open');
+});
+
+//console.log("Hello World");
+//document = "getdates.js";
+let yr = new Date().getFullYear();
+let lastModified = document.lastModified;
+let author = "Jamie Cummings";
+let place = "Utah, USA";
+
+// \u00A9 is the unicode for the copywrite symbol
+document.getElementById("copywrite").innerHTML = `\u00A9 ${yr} | ${author} | ${place}`;
+document.getElementById("modified").innerHTML = `Last Modified ${lastModified}`;
