c3887cde429d3a6717c6db6dc1a2016e8d2215a0
other-world <jamie@other-world.org>
2024-07-12 15:23:24 -0600

    finally updated variable type on javascript footer to be more aligned with standards.

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,9 +1,9 @@
 console.log("Hello World");
 //document = "getdates.js";
-let yr = new Date().getFullYear();
-let lastModified = document.lastModified;
-let author = "Jamie Cummings";
-let place = "Utah, USA";
+const yr = new Date().getFullYear();
+const lastModified = document.lastModified;
+const author = "Jamie Cummings";
+const place = "Utah, USA";
 
 // \u00A9 is the unicode for the copywrite symbol
 document.getElementById("copywrite").innerHTML = `\u00A9 ${yr} | ${author} | ${place}`;
